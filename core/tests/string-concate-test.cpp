5a10e39d7766695550d7a95b373a813320e8c672
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2017-05-07 13:30:08 +0200

    Fixed std::bad_alloc crash after long run of StringConcateTest.

--- core/tests/string-concate-test.cpp
+++ core/tests/string-concate-test.cpp
@@ -5,6 +5,10 @@ using namespace Test;
 Enum::TestType StringConcateTest::type() const { return Enum::TestType::StringConcate; }
 
 u64 StringConcateTest::execute(u64 in) {
+
+	if (m_concate.size() > 32768)
+		m_concate = "";
+
 	m_concate += std::to_string(in);
 	m_concate += std::to_string(in);
 	m_concate += std::to_string(in);
